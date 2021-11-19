94cdb3c4e00305ea0304fd4815eb7965549b389a
Jeni Dimitrova <jenidimitrova@Jenis-MacBook-Air.local>
2021-11-19 13:24:14 +0100

    update front end to take data from api

--- src/app/pages/lake/lake.page.ts
+++ src/app/pages/lake/lake.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Trail } from 'src/app/models/trail.model';
+import { TrailService } from 'src/app/services/trail.service';
 
 @Component({
   selector: 'app-lake',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LakePage implements OnInit {
 
-  constructor() { }
+  trail = {
+    name: '',
+    description: '',
+    length: ''
+  };
+  trails: Trail[];
+
+  currentTrail = null;
+  currentIndex = -1;
+
+  name = '';
+  message = '';
+
+  constructor(private trailService: TrailService) { }
 
   ngOnInit() {
+    this.retrieveTrails();
+    this.message = '';
+  }
+
+  //get all trails
+retrieveTrails() {
+  this.trailService.getAll()
+    .subscribe(
+      data => {
+        this.trails = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
+}
+//refresh list
+  refreshList() {
+    this.retrieveTrails();
+    this.currentTrail = null;
+    this.currentIndex = -1;
+  }
+
+  setActiveTrail(trail, index) {
+    this.currentTrail = trail;
+    this.currentIndex = index;
   }
 
 }
